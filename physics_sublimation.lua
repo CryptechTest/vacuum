2af7700b61bc462a65da2b02d3ae7a8e132cf4e2
Jon <squid@sqdmc.net>
2023-04-08 04:08:03 -0400

    update for plants; fix atmos for sublimation..?

--- physics_sublimation.lua
+++ physics_sublimation.lua
@@ -1,30 +1,22 @@
-
-
 -- sublimate nodes in vacuum
 minetest.register_abm({
-	label = "space vacuum sublimate",
-	nodenames = {"group:snowy", "group:leaves", "group:water"},
-	neighbors = {"vacuum:vacuum"},
-	interval = 1,
-	chance = 1,
-	min_y = vacuum.space_height,
-	action = vacuum.throttle(100, function(pos)
-		if not vacuum.is_pos_in_space(pos) or vacuum.near_powered_airpump(pos) then
-			return
-		end
+    label = "space vacuum sublimate",
+    nodenames = {"group:snowy", "group:leaves", "group:water"},
+    neighbors = {"vacuum:vacuum"},
+    interval = 1,
+    chance = 1,
+    min_y = vacuum.space_height,
+    action = vacuum.throttle(100, function(pos)
+        if not vacuum.is_pos_in_space(pos) or vacuum.near_powered_airpump(pos) then
+            return
+        end
 
-		--if (vacuum.near_atmos(pos)) thenz
-		--	return
-		--end
-		--if (vacuum.near_air(pos)) then
-		--	return
-		--end
-
-		--minetest.set_node(pos, {name = "vacuum:vacuum"})
-
-		if (not vacuum.near_atmos(pos, 26)) and (not vacuum.near_air(pos, 25)) then
-			--minetest.set_node(pos, {name = "vacuum:vacuum"})
-			minetest.set_node(pos, {name = "asteroid:atmos"})
-		end
-	end)
+        if (not vacuum.near_atmos(pos, 26)) and (not vacuum.near_air(pos, 25)) then
+            -- minetest.set_node(pos, {name = "vacuum:vacuum"})
+            -- minetest.set_node(pos, {name = "asteroid:atmos"})
+            minetest.set_node(pos, {
+                name = "vacuum:atmos_thin"
+            })
+        end
+    end)
 })
