e2636861d19b769753005a9bff058b768e7605af
squidicuzz <squid@cryptech.services>
2025-10-20 03:24:01 -0400

    add air bottle node

--- airbottle.lua
+++ airbottle.lua
@@ -17,3 +17,21 @@ if minetest.get_modpath("unified_inventory") then
         width = 0
     })
 end
+
+minetest.register_node("vacuum:air_bottle", {
+	description = "Air Bottle",
+	drawtype = "plantlike",
+	tiles = {vacuum.air_bottle_image},
+	inventory_image = vacuum.air_bottle_image,
+	wield_image = vacuum.air_bottle_image,
+	paramtype = "light",
+	is_ground_content = false,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
+	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
+	sounds = default.node_sound_water_defaults(),
+    drop = "",
+})
